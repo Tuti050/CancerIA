288571df7818997ab9acb9f1b53c31465e8859bb
Larissa Campos <larissacamposcardoso@gmail.com>
2025-04-14 16:47:49 -0300

    ta indooo

--- app/main.py
+++ app/main.py
@@ -10,13 +10,19 @@ def main ():
         
         )
     
-    with open("assets/style.css") as f:
-        st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-    
     with st.container():
         st.title("Welcome to CancerIA")
         st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytology lab. You can also update the measurements by hand using the sliders in the sidebar")
-
     
+
+    col1, col2 = st.columns([4,1])
+    
+    with col1:
+        st.write("this is column 1")
         
+    with col2:
+        st.write("this is column 2")
+    
+if __name__ == "__main__":
+    main()
     
