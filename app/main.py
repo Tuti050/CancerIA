3016fe675746e43fda6f2c9f627de1ccfe2773b6
Larissa Campos <larissacamposcardoso@gmail.com>
2025-04-30 14:05:32 -0300

    titulo no topo

--- app/main.py
+++ app/main.py
@@ -20,19 +20,15 @@ from model.mainmodel import extrair_medidas_da_imagem
 
 def get_clean_data():
     data = pd.read_csv("data/data.csv")
-    
     data = data.drop(['Unnamed: 32', 'id'], axis=1)
-    
-    data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
-    
+    data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
     return data
 
 
 def add_sidebar():
     st.sidebar.header(" Measurements")
-    
     data = get_clean_data()
-    
+
     slider_labels = [
         ("Radius (mean)", "radius_mean"),
         ("Texture (mean)", "texture_mean"),
@@ -69,47 +65,44 @@ def add_sidebar():
     input_dict = {}
 
     for label, key in slider_labels:
-        
-        value = st.session_state.get(key, float(data[key].mean())) 
+        value = st.session_state.get(key, float(data[key].mean()))
         input_dict[key] = st.sidebar.slider(
-        label,
-        min_value=float(0),
-        max_value=float(data[key].max()),
-        value=float(data[key].mean())
+            label,
+            min_value=float(0),
+            max_value=float(data[key].max()),
+            value=float(data[key].mean())
         )
-    
+
     return input_dict
 
 
 def get_scaled_values(input_dict):
     data = get_clean_data()
-    
     X = data.drop(['diagnosis'], axis=1)
-    
+
     scaled_dict = {}
-    
+
     for key, value in input_dict.items():
         max_val = X[key].max()
         min_val = X[key].min()
         scaled_value = (value - min_val) / (max_val - min_val)
         scaled_dict[key] = scaled_value
-    
+
     return scaled_dict
-    
+
 
 def get_radar_chart(input_data):
-    
     input_data = get_scaled_values(input_data)
-    
-    categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
-                'Smoothness', 'Compactness', 
+
+    categories = ['Radius', 'Texture', 'Perimeter', 'Area',
+                'Smoothness', 'Compactness',
                 'Concavity', 'Concave Points',
                 'Symmetry', 'Fractal Dimension']
 
     fig = go.Figure()
 
     fig.add_trace(go.Scatterpolar(
-        r=[ 
+        r=[
             input_data['radius_mean'], input_data['texture_mean'], input_data['perimeter_mean'],
             input_data['area_mean'], input_data['smoothness_mean'], input_data['compactness_mean'],
             input_data['concavity_mean'], input_data['concave points_mean'], input_data['symmetry_mean'],
@@ -119,16 +112,18 @@ def get_radar_chart(input_data):
         fill='toself',
         name='Mean Value'
     ))
+
     fig.add_trace(go.Scatterpolar(
         r=[
             input_data['radius_se'], input_data['texture_se'], input_data['perimeter_se'], input_data['area_se'],
             input_data['smoothness_se'], input_data['compactness_se'], input_data['concavity_se'],
-            input_data['concave points_se'], input_data['symmetry_se'],input_data['fractal_dimension_se']
+            input_data['concave points_se'], input_data['symmetry_se'], input_data['fractal_dimension_se']
         ],
         theta=categories,
         fill='toself',
         name='Standard Error'
     ))
+
     fig.add_trace(go.Scatterpolar(
         r=[
             input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
@@ -142,82 +137,83 @@ def get_radar_chart(input_data):
     ))
 
     fig.update_layout(
-    polar=dict(
-        radialaxis=dict(
-        visible=True,
-        range=[0, 1]
-        )),
-    showlegend=True
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0, 1]
+            )),
+        showlegend=True
     )
-    
+
     return fig
 
 
 def add_predictions(input_data):
     model = pickle.load(open("model/model.pkl", "rb"))
     scaler = pickle.load(open("model/scaler.pkl", "rb"))
-    
+
     input_array = np.array(list(input_data.values())).reshape(1, -1)
-    
     input_array_scaled = scaler.transform(input_array)
-    
     prediction = model.predict(input_array_scaled)
-    
+
     st.subheader("Cell cluster prediction")
     st.write("The cell cluster is:")
-    
+
     if prediction[0] == 0:
         st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
     else:
         st.write("<span class='diagnosis malicious'>Malicious</span>", unsafe_allow_html=True)
-    
-    
+
     st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
     st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
-    
-    st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
+    st.write("This app can assist medical professionals in making a diagnosis, "
+            "but should not be used as a substitute for a professional diagnosis.")
+
+
+def atualizar_measurements(medidas: dict):
+    st.session_state.radius_mean = medidas["radius_mean"]
+    st.session_state.texture_mean = medidas["texture_mean"]
+    st.session_state.perimeter_mean = medidas["perimeter_mean"]
+
+    for key, value in medidas.items():
+        if key not in st.session_state:
+            st.session_state[key] = value
 
 
 def main():
     with open("assets/style.css") as f:
         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-    
+
+    st.title("MamamIA")
+    st.write("Please connect this app to your cytology lab to help diagnose breast cancer from your tissue sample. "
+            "This app predicts using a machine learning model whether a breast mass is benign or malignant based "
+            "on the measurements it receives from your cytosis lab. You can also update the measurements by hand "
+            "using the sliders in the sidebar.")
+
+    uploaded_image = st.file_uploader("Envie uma imagem de mamografia", type=["jpg", "png", "jpeg"])
+
+    if st.button("Usar imagem", key="stFloatingButton"):
+        if uploaded_image:
+            image = Image.open(uploaded_image)
+            try:
+                medidas_extraidas = extrair_medidas_da_imagem(image)
+                atualizar_measurements(medidas_extraidas)
+                st.success(f"Medidas extraídas da imagem: {medidas_extraidas}")
+            except Exception as e:
+                st.error(f"Erro ao processar imagem: {e}")
+        else:
+            st.warning("Por favor, envie uma imagem antes de usar esse botão.")
+
     input_data = add_sidebar()
-    
-    with st.container():
-        st.title("MamamIA")
-        st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
-    
-    col1, col2 = st.columns([4,1])
-    
+
+    col1, col2 = st.columns([4, 1])
+
     with col1:
         radar_chart = get_radar_chart(input_data)
         st.plotly_chart(radar_chart)
     with col2:
         add_predictions(input_data)
 
-def atualizar_measurements(medidas: dict):
-    st.session_state.radius_mean = medidas["radius_mean"]
-    st.session_state.texture_mean = medidas["texture_mean"]
-    st.session_state.perimeter_mean = medidas["perimeter_mean"]
-    
-    for key, value in medidas.items():
-        if key not in st.session_state:
-            st.session_state[key] = value
-
-uploaded_image = st.file_uploader("Envie uma imagem de mamografia", type=["jpg", "png", "jpeg"])
-
-if st.button("Usar imagem", key="stFloatingButton"):
-    if uploaded_image:
-        image = Image.open(uploaded_image)
-        try:
-            medidas_extraidas = extrair_medidas_da_imagem(image)
-            atualizar_measurements(medidas_extraidas)  
-            st.success(f"Medidas extraídas da imagem: {medidas_extraidas}")
-        except Exception as e:
-            st.error(f"Erro ao processar imagem: {e}")
-    else:
-        st.warning("Por favor, envie uma imagem antes de usar esse botão.")
 
 if __name__ == '__main__':
     main()
